87c6651e32bba73efd0d6bb80de1cf69fc5376e6
nr_dev <nelson_rojas_janda@hotmail.com>
2021-11-18 14:58:35 +0100

    Added nav bar for mobile

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Variables
+nav = document.getElementById('nav');
+bgMenu = document.getElementById('back__menu');
+
+// Event on bar click
+document.getElementById('btn__menu').addEventListener('click', showMenu);
+document.getElementById('back__menu').addEventListener('click', hideMenu);
+
+function showMenu () {
+    nav.style.right = "0";
+    bgMenu.style.left = "0";
+}
+
+function hideMenu () {
+    nav.style.right = "-50%"
+    bgMenu.style.left = "-100%";
+}
