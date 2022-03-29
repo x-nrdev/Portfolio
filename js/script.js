2b6ee0a546ffae44ed772e8b076430c7d445f372
nr_dev <nelson_rojas_janda@hotmail.com>
2022-03-29 09:06:50 +0200

    Add scripts to change language

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 // Variables
-nav = document.getElementById('nav');
-bgMenu = document.getElementById('back__menu');
+const nav = document.getElementById('nav');
+const bgMenu = document.getElementById
+('back__menu');
+const lang = document.forms['form']['lang'];
 
 // Event on bar click
 document.getElementById('btn__menu').addEventListener('click', showMenu);
 document.getElementById('back__menu').addEventListener('click', hideMenu);
+
+// Events of language
+lang.addEventListener('change', (e) => {
+    changeLanguage(e.target.value);
+})
 
 // Functions
 function showMenu () {
@@ -15,4 +22,11 @@ function showMenu () {
 function hideMenu () {
     nav.style.right = "-50%"
     bgMenu.style.left = "-100%";
+}
+
+async function changeLanguage (language) {
+    const requestJson = await fetch(`../languages/${language}.json`);
+    const txt = await requestJson.json();
+
+    console.log (txt);
 }
