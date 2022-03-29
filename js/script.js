96cb108b787c955a8c5be7e95c88ab0ea748d5d5
nr_dev <nelson_rojas_janda@hotmail.com>
2022-03-29 22:08:59 +0200

    Change the way to change language

--- js/script.js
+++ js/script.js
@@ -26,38 +26,9 @@ function hideMenu () {
 }
 
 async function changeLanguage (language) {
-    const requestJson = await fetch(`../languages/${language}.json`);
-    const txt = await requestJson.json();
-
-    for (let i of textsToChange) {
-        const section = i.dataset.section;
-        const value = i.dataset.value;
-
-        if (section !== 'header') {
-            i.innerHTML = txt[section][value];
-        } else if (language === 'en') {
-            i.innerHTML = `
-                Nelson Rojas
-                <br>
-                <span class="header__card__text-fe">
-                    Frontend
-                </span>
-                <span class="header__card__text-dw" data-section="header" data-value="dev">
-                    ${txt[section][value]}
-                </span>
-            `;
-        } else {
-            i.innerHTML = `
-                Nelson Rojas
-                <br>
-                <span class="header__card__text-dw" data-section="header" data-value="dev">
-                    ${txt[section][value]}
-                </span>
-                <span class="header__card__text-fe">
-                    Frontend
-                </span>
-            `;
-        }
-    };
-    // console.log (txt);
+    if (language === 'en') {
+        location.href = "index-en.html"
+    } else {
+        location.href = "index.html"
+    }
 }
