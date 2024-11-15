75889a759f5cf68a7a2e2befaa749d1fc36a8d0f
Nelson Rojas <nelson_rojas_janda@hotmail.com>
2024-11-15 14:52:17 +0100

    Add View Full Resume anchor to CVs

--- src/types.d.ts
+++ src/types.d.ts
@@ -14,6 +14,7 @@ export interface ProjectType extends ArticleType {
 export interface AnchorTextType {
     href: string
     target?: string
+    class?: string
 }
 
 export interface SocialNetworkType {
